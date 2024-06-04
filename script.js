f676a5f50421080c56ad74491f510c0c97f6bc3f
John Paul Soria <142276915+JohnPaulYSoria@users.noreply.github.com>
2024-06-04 15:19:01 +0800

    initial commit

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 function validateLogin() {
-    var username = document.getElementById("username").value;
-    var password = document.getElementById("password").value;
-  
-    if(username === "admin002" && password === "admin123") {
-      alert("Log in successfully");
-      window.location.assign("dashboard.html");
-      return false;
-    } else {
-      alert("Invalid information");
-      return false;
-    }
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+
+  if(username === "admin" && password === "admin") {
+    window.location.assign("dashboard.html");
+    return false;
+  } else {
+    alert("Invalid information");
+    return false;
   }
-  
+}
--- scriptd.js
+++ scriptd.js
@@ -1,8 +1,7 @@
 function openMenu() {
-    document.getElementById("menu").style.width = "250px";
-  }
-  
-  function closeMenu() {
-    document.getElementById("menu").style.width = "0";
-  }
-  
+  document.getElementById("menu").style.width = "250px";
+}
+
+function closeMenu() {
+  document.getElementById("menu").style.width = "0";
+}
